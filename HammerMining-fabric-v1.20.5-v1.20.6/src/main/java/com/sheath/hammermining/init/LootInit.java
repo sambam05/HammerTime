4e1cf1bb0a4706445f824256564c45cce42c7395
sambam05 <65609321+sambam05@users.noreply.github.com>
2025-12-21 17:54:28 +0000

    chore: remove old v1.20 fabric bundles

--- HammerMining-fabric-v1.20.5-v1.20.6/src/main/java/com/sheath/hammermining/init/LootInit.java
+++
@@ -1,8 +0,0 @@
-package com.sheath.hammermining.init;
-
-public class LootInit {
-
-    public static void register() {
-        // Loot injection is skipped for 1.20.x to keep compatibility simple.
-    }
-}
