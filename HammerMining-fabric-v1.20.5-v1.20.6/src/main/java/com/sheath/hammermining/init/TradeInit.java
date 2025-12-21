6f4684adba6d5304382757798a83cae61790d58d
sambam05 <65609321+sambam05@users.noreply.github.com>
2025-12-21 17:54:28 +0000

    chore: remove old v1.20 fabric bundles

--- HammerMining-fabric-v1.20.5-v1.20.6/src/main/java/com/sheath/hammermining/init/TradeInit.java
+++
@@ -1,7 +0,0 @@
-package com.sheath.hammermining.init;
-
-public class TradeInit {
-    public static void register() {
-        // Trades are skipped for 1.20.x in this compatibility build.
-    }
-}
